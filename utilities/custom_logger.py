e9ca7fb0d134401da11f9568637a938eeec3c10a
alexaiung <110355804+alexaiung@users.noreply.github.com>
2023-05-30 22:24:57 -0300

    SeleniumDriver
    Created classes and they are working partially.

--- utilities/custom_logger.py
+++ utilities/custom_logger.py
@@ -1,3 +1,15 @@
+from datetime import datetime
+import os
+
 class CustomLogger():
     def __init__(self, log):
         self.log = log
+
+    @staticmethod
+    def info(text):
+        filename = datetime.now().strftime('%Y%m%d')
+        #os.makedirs(f'logs/{filename}.txt')
+        with open(f'logs/{filename}.txt', 'a', encoding="utf-8") as file:
+            file.write(f"{datetime.now().strftime('%Y/%m/%d %H:%M:%S')} - {text}")
+            file.write('\n\n')
+            
